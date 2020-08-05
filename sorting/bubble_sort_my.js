193044a4aef1ba24f0c3e10c4eb0a012a95f5b56
Gaurav Subedi <subedigaurav20@gmail.com>
2020-08-05 14:45:50 +0545

    add (algo): add bubblesort, selectionsort and insertionsort

---
+++ sorting/bubble_sort_my.js
@@ -0,0 +1,15 @@
+//! my implementation of bubble sort
+// one of the elementary sort algorithms
+function bubbleSort(arr) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = 0; j < arr.length - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        ;[arr[j], arr[j + 1]] = [arr[j + 1], arr[j]]
+      }
+    }
+  }
+}
+
+let arr = [5, 6, 77, 11, 61, 6, 1]
+bubbleSort(arr)
+console.log(arr)
---
+++ sorting/selection_sort_my.js
@@ -0,0 +1,21 @@
+const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0]
+
+function selectionSort(arr) {
+  // start at the first element
+  for (let i = 0; i < arr.length - 1; i++) {
+    // find the minimum element from the subarray
+    let min_index = i
+    // find the index of the minumum element
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[min_index]) {
+        min_index = j
+      }
+    }
+    // place the miminum value in its correct position
+    // ignore the semicolon, ok?
+    ;[arr[i], arr[min_index]] = [arr[min_index], arr[i]]
+  }
+}
+
+selectionSort(numbers)
+console.log(numbers)
