51c147079b73386502cf7cfc83a01fa105641e80
subedigaurav <ergauravsubedi@gmail.com>
2020-08-08 14:08:46 +0545

    minor: prettify files and remove semicolon

---
+++ graph_search/bfs.js
@@ -0,0 +1,2 @@
+const graph = require("../graphs/graphs_mysoln");
+const queue = require("../queue/queue_ll");
--- queue/queue_ll.js
+++ queue/queue_ll.js
@@ -2,76 +2,76 @@
 
 class Node {
   constructor(value) {
-    this.value = value;
-    this.next = null;
+    this.value = value
+    this.next = null
   }
 }
 
 class Queue {
   constructor() {
-    this.first = null;
-    this.last = null;
-    this.length = 0;
+    this.first = null
+    this.last = null
+    this.length = 0
   }
 
   peek() {
-    return this.first;
+    return this.first
   }
 
   enqueue(value) {
-    const newNode = new Node(value);
+    const newNode = new Node(value)
 
     // if the queue is empty
     if (this.length == 0) {
-      this.first = newNode;
-      this.last = newNode;
+      this.first = newNode
+      this.last = newNode
     } else {
-      this.last.next = newNode;
-      this.last = newNode;
+      this.last.next = newNode
+      this.last = newNode
     }
 
-    this.length++;
-    return this;
+    this.length++
+    return this
   }
 
   // removes from the front of the queue
   dequeue(value) {
     // if the queue is empty
     if (!this.first) {
-      return null;
+      return null
     }
     // if there is only one item, the last pointer is null after dequeue
     if (this.first == this.last) {
-      this.last = null;
+      this.last = null
     }
-    const dequeuedItem = this.first;
-    this.first = this.first.next;
-    this.length--;
-    return dequeuedItem;
+    const dequeuedItem = this.first
+    this.first = this.first.next
+    this.length--
+    return dequeuedItem
   }
 
   isEmpty() {
-    return this.length === 0;
+    return this.length === 0
   }
 
   print() {
-    let temp = this.first;
+    let temp = this.first
     while (temp !== null) {
-      console.log(temp.value);
-      temp = temp.next;
+      console.log(temp.value)
+      temp = temp.next
     }
-    console.log();
+    console.log()
   }
 }
 
-const myQueue = new Queue();
-myQueue.enqueue("Joy");
-myQueue.enqueue("Matt");
-myQueue.enqueue("Pavel");
-myQueue.enqueue("Samir");
-myQueue.print();
-myQueue.peek();
-myQueue.dequeue();
-myQueue.dequeue();
-myQueue.dequeue();
-myQueue.print();
+const myQueue = new Queue()
+myQueue.enqueue('Joy')
+myQueue.enqueue('Matt')
+myQueue.enqueue('Pavel')
+myQueue.enqueue('Samir')
+myQueue.print()
+myQueue.peek()
+myQueue.dequeue()
+myQueue.dequeue()
+myQueue.dequeue()
+myQueue.print()
