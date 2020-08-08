2b6a3e7b14e19dc8496c3c8dac7693864988885f
Gaurav Subedi <subedigaurav20@gmail.com>
2020-08-08 14:08:46 +0545

    minor: prettify files and remove semicolon

--- searching/binary_search.js
+++ searching/binary_search.js
@@ -4,23 +4,23 @@
  * @param {integer} t the element to be searched
  */
 function binarySearchI(A, target) {
-  const n = A.length
-  let l = 0
-  let r = n - 1
+  const n = A.length;
+  let l = 0;
+  let r = n - 1;
 
-  let m = null
+  let m = null;
 
   while (l <= r) {
-    m = Math.floor((l + r) / 2)
+    m = Math.floor((l + r) / 2);
     if (A[m] < target) {
-      l = m + 1
+      l = m + 1;
     } else if (A[m] > target) {
-      r = m - 1
+      r = m - 1;
     } else {
-      return true
+      return true;
     }
   }
-  return false
+  return false;
 }
 
 /**
@@ -32,27 +32,27 @@ function binarySearchI(A, target) {
  */
 function binarySearchR(A, l, r, t) {
   if (l <= r) {
-    let mid = Math.floor((l + r) / 2)
+    let mid = Math.floor((l + r) / 2);
 
     //# if the middle element is the one to be searched
     if (A[mid] == t) {
-      return true
+      return true;
     }
 
     //# if the element is smaller than the mid, it is present in the left subarray
     else if (A[mid] > t) {
-      return binarySearchR(A, l, mid - 1, t)
+      return binarySearchR(A, l, mid - 1, t);
     }
 
     //# else the element is presnet in the right subarray
-    else return binarySearchR(A, mid + 1, r, t)
+    else return binarySearchR(A, mid + 1, r, t);
   }
 
   //# we reach here if the element is not present in the array
-  return false
+  return false;
 }
 
-const numbers = [5, 12, 45, 78, 98, 123, 125, 188, 240, 250]
-let foundI = binarySearchI(numbers, 55)
-let foundR = binarySearchR(numbers, 0, numbers.length - 1, 55)
-console.log(foundI, foundR)
+const numbers = [5, 12, 45, 78, 98, 123, 125, 188, 240, 250];
+let foundI = binarySearchI(numbers, 55);
+let foundR = binarySearchR(numbers, 0, numbers.length - 1, 55);
+console.log(foundI, foundR);
--- searching/linear_search.js
+++ searching/linear_search.js
@@ -1,27 +1,27 @@
-const beasts = ['Centaur', 'Godzilla', 'Mosura', 'Minotaur', 'Hydra', 'Nessie']
+const beasts = ["Centaur", "Godzilla", "Mosura", "Minotaur", "Hydra", "Nessie"];
 
 // built-in method (uses linear search)
-beasts.indexOf('Godzilla')
+beasts.indexOf("Godzilla");
 
 // another build-in method, return index
 beasts.findIndex(function (item) {
-  return item == 'Godzilla'
-})
+  return item == "Godzilla";
+});
 
 // returns the item if a match is found
 beasts.find(function (item) {
-  return item == 'Godzilla'
-})
+  return item == "Godzilla";
+});
 
-beasts.includes('Godzilla')
+beasts.includes("Godzilla");
 
 // naive implementation of linear search
-let target = 'Godzilla'
-let found = false
+let target = "Godzilla";
+let found = false;
 for (let i = 0; i < beasts.length; i++) {
   if (beasts[i] == target) {
-    found = true
-    break
+    found = true;
+    break;
   }
 }
-console.log(found)
+console.log(found);
